d11ba35887e932f61bccd29cf8c7953288c05c2a
LACSTUDIO <lican2935@gmail.com>
2025-02-22 23:23:03 +0800

    Merge branch 'main' of https://gitee.com/lacsgf/api

---
+++ js/umami-home.js
@@ -0,0 +1,41 @@
+export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+  
+    try {
+      const { query: { websiteId }, method } = req;
+  
+      if (method !== 'GET') {
+        return res.status(405).json({ error: 'Method not allowed' });
+      }
+  
+      const token = process.env.API_TOKEN; // 从环境变量中获取 token
+      const umiId = websiteId || 'a4e8c20f-d2e8-4b10-bdf5-2d52c389fd45';
+      const umiTime = Date.now();
+  
+      const umiUrl = `https://umami.xn--5brr03o.top/api/websites/${umiId}/stats?startAt=0&endAt=${umiTime}`;
+  
+      const response = await fetch(umiUrl, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: `API Error: ${error.message}` });
+    }
+  }
